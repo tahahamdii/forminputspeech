6b526f115650fb4b65eb47e1062beb29a6ed3dca
Med Taha Hamdi <hamdi.taha@esprit.tn>
2023-05-29 20:02:39 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+function init() {
+    window.SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    if ("SpeechRecognition" in window || "webkitSpeechRecognition" in window) {
+      let speech = {
+        enabled: true,
+        listening: false,
+        recognition: new window.SpeechRecognition(),
+        text: ""
+      };
+      speech.recognition.continuous = true;
+      speech.recognition.interimResults = true;
+      speech.recognition.lang = "en-US";
+      speech.recognition.addEventListener("result", (event) => {
+        const audio = event.results[event.results.length - 1];
+        speech.text = audio[0].transcript;
+        const tag = document.activeElement.nodeName;
+        if (tag === "INPUT" || tag === "TEXTAREA") {
+          if (audio.isFinal) {
+            document.activeElement.value += speech.text;
+          }
+        }
+        result.innerText = speech.text;
+      });
+  
+      toggle.addEventListener("click", () => {
+        speech.listening = !speech.listening;
+        if (speech.listening) {
+          toggle.classList.add("listening");
+          toggle.innerText = "Listening ...";
+          speech.recognition.start();
+        } else {
+          toggle.classList.remove("listening");
+          toggle.innerText = "Toggle listening";
+          speech.recognition.stop();
+        }
+      });
+    }
+    const submitButton = document.getElementById("submit");
+    const formInputs = document.querySelectorAll("input, textarea");
+    
+    submitButton.addEventListener("click", () => {
+      // Handle form submission
+      // You can perform any necessary processing here
+      
+      // Display a confirmation message
+      result.innerText = "Form submitted. Thanks, doctor!";
+      
+      // Clear the form fields
+      formInputs.forEach((input) => {
+        input.value = "";
+      });
+    });
+  }
+  init();
+  
